ae87e49a8da7f2009d13fa3a01747ce516cea3b4
Vineet Kumar Singh <vineetsingh.next@gmail.com>
2026-09-23 13:57:39 +0530

    front end also udpated

---
+++ Frontend-React/src/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router'
+
+export default function Navbar() {
+  return (
+    <div className='flex justify-center mx-auto gap-5'>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/about'}>About</NavLink>
+        <NavLink to={'/contact'}>Contact Us</NavLink>
+        <NavLink to={'/services'}>Our Services</NavLink>
+        <NavLink to={'/register'}>Register Here</NavLink>
+        <NavLink to={'/login'}>Login</NavLink>
+    </div>
+  )
+}
---
+++ Frontend-React/src/components/userProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function userProfile() {
+  return (
+    <div>userProfile</div>
+  )
+}
