d3b276a9f0b2e295d4444d6994120d074191784f
Vineet Kumar Singh <vineetsingh.next@gmail.com>
2026-09-25 21:40:20 +0530

    frontend styleform

--- Frontend-React/src/components/Registration.tsx
+++ Frontend-React/src/components/Registration.tsx
@@ -1,31 +1,69 @@
-import React from 'react'
-// import { Button } from './ui/button'
-import {Button, Card} from './uiDesign/button'
-import { Button as Btn } from './ui/button';
-import { Card as MyCard,CardHeader,CardTitle,CardDescription,CardFooter,CardContent } from './ui/card';
 
 
 export default function Registration() {
   return (
-    <div className='flex justify-center flex-col w-md items-center'>
-        <h1>Registration Form</h1>
-        <Button>Register</Button>
-<MyCard className='min-w-sm text-center'>
-        <CardHeader className='font-bold text-lg border bg-gray-200'>
-            This is Header
-        </CardHeader>
-       <CardTitle>Title</CardTitle>
-       <CardDescription>
-        this is about the CardDescription
-       </CardDescription>
-       <CardContent>this is card content</CardContent>
-       <CardFooter>
-        <Btn>button1</Btn>
-        <Btn>button2</Btn>
-       </CardFooter>
-</MyCard>
+    <div className='flex justify-center flex-col  items-center'>
+      
 
-        
+
+
+      <div>
+        <div>
+          <h1>Registration Form</h1>
+          <form>
+            <div className="grid gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="firstname">First Name</label>
+                <input className="border border-gray-400 rounded px-2 py-1.5"/>
+              </div>
+               <div className="flex flex-col">
+                <label htmlFor="lastname">Last Name</label>
+                <input className="border border-gray-400 rounded px-2 py-1.5"/>
+              </div>
+               <div className="flex flex-col">
+                <label htmlFor="date" >Date of Birth</label>
+                <input type="date"  className="border border-gray-400 rounded px-2 py-1.5"/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="select" >Gender</label>
+                <select className="border border-gray-400 rounded px-2 py-1.5">
+                  <option value="gender">--Select Gender--</option>
+                  <option value="gender">Male</option>
+                  <option value="gender">Female</option>
+                  <option value="gender">Other</option>
+                </select>
+               
+              </div>
+                <div className="flex flex-col gap-2">
+                <label htmlFor="date" >Address</label>
+                <input type="text" placeholder="address 1"  className="border border-gray-400 rounded px-2 py-1.5"/>
+                <input type="text" placeholder="address 2"  className="border border-gray-400 rounded px-2 py-1.5"/>
+                <input type="text" placeholder="address 3"  className="border border-gray-400 rounded px-2 py-1.5"/>
+                <input type="number" placeholder="pincode"  className="border border-gray-400 rounded px-2 py-1.5"/>
+
+              </div>
+
+              <div className="flex flex-col">
+                <label htmlFor="date" >Aadhaar Number</label>
+                <input type="number" placeholder="aadhaar number"  className="border border-gray-400 rounded px-2 py-1.5"/>
+              </div>
+
+              <div className="flex flex-col">
+                <label htmlFor="date" >Aadhaar Number</label>
+                <input type="number" placeholder="aadhaar number"  className="border border-gray-400 rounded px-2 py-1.5"/>
+              </div>
+              
+              
+            </div>
+          </form>
+        </div>
+      </div>
+
+
+
+
+
+
     </div>
   )
 }
--- Frontend-React/src/components/login.tsx
+++ Frontend-React/src/components/login.tsx
@@ -1,60 +1,35 @@
-import { useEffect, useState } from 'react'
-import type {IDepartmentOption} from '../types/employee'
-
-export default function login() {
-  const [departments, setDepartments] = useState<IDepartmentOption[]>([])
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+import { CardFooter } from "./ui/card"
 
 
-  useEffect (()=>{
-    const fetchDepartments = async () =>{
-      try {
-        const response = await fetch('http://localhost:8080/api/departments')
-        if (!response.ok){
-          throw new Error(`HTTP ERROR STATUS: ${response.status}`)
-        }
-        const result = await response.json();
-        setDepartments(result.data)
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'Failed to load departments')
-      } finally {
-        setLoading(false)
-      }
-
-    
-    }
-    fetchDepartments();
-  },[])
+export default function Login() {
   return (
-    <div className='text-center flex mx-auto justify-center flex-col w-1/2'>
+    <div className="item-center justify-center flex bg-slate-800">
+      <div className="flex sm:min-w-md items-center justify-center h-screen ">
+        <form className="border px-4 py-2 rounded-md border-gray-500 bg-white shadow-xl min-w-md">
+          <div className="grid grid-cols-1 gap-4">
+            <h1 className="text-center font-bold text-xl border-b-2 ">Login form</h1>
 
-      <h1 >List of departments</h1>
-
-      <select name="departments" id="department">
-
-        <option value="department">--select department--
-             {loading && <p>Loading departments...</p>}
-
-        </option>
-
+         <div className=" flex flex-col mt-4">
+           <label htmlFor="email">Email</label>
+           <input type="text" placeholder="email"className="border px-4 py-1.5 rounded border-gray-300 focus:ring-2 focus:ring-gray-400" />
+          </div>
+            <div className=" flex flex-col">
+           <label htmlFor="email">Password</label>
+           <input type="text" placeholder="password"className="border px-4 py-1.5 rounded border-gray-300 focus:ring-2 focus:ring-gray-400" />
+          </div>
+         
+<div className="flex justify-between px-4 my-4 bg-gray-200 py-3 ">
+        
+            <button type="reset" className="border border-gray-600 px-4 py-1.5 rounded">Reset</button>
+            <button className="border border-blue-600 px-4 py-1.5 rounded">Submit</button>
+</div>
+          
       
-
-      {departments.map((dep)=>(
-        <option key={dep._id}>{dep.departmentName}</option>
-      ))}
-
-
-      </select>
-      {error && <p>{error}</p>}
-
-      {/* <button className='btn-primary'>Submit</button> */}
-
-   
-
-   
-   
-    
+         </div>
+        </form>
+      </div>
+      
     </div>
   )
 }
+ 
